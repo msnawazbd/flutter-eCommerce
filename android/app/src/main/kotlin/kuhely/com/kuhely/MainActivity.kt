2b5ba0d2c32ab781faca64fc58b6816e9fa57b14
MS Nawaz <msnawazbd@gmail.com>
2022-02-01 17:55:25 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/kuhely/com/kuhely/MainActivity.kt
@@ -0,0 +1,6 @@
+package kuhely.com.kuhely
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:kuhely/pages/home_page.dart';
+import 'package:kuhely/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login" : (context) => LoginPage(),
+      },
+    );
+
+  }
+}
