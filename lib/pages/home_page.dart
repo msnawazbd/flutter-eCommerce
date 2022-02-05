403c2768752ba9f239c2f1dc69a261af918337cf
MS Nawaz <msnawazbd@gmail.com>
2022-02-05 18:14:36 +0600

    Added Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,60 @@
-import 'package:flutter/material.dart';
-import 'package:kuhely/widgets/drawer.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kuhely/models/catalog.dart';
+import 'package:kuhely/widgets/drawer.dart';
+import 'package:kuhely/widgets/items_widget.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData['products'];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Kuhely"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to Kuhely!"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                //itemCount: dummyList.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(item: CatalogModel.items[index]
+                      //item: dummyList[index]
+                      );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
